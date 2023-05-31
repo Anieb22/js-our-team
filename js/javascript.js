9dc5c63a576ae678aff054911003990637baf6ee
Anieb22 <anielloverdino@gmail.com>
2023-06-01 01:38:44 +0200

    trasformati gli oggetti in un array di oggetti

--- js/javascript.js
+++ js/javascript.js
@@ -1,42 +1,39 @@
-let person1 = {
-  name: 'Waine Barnett',
-  role: 'Founder & CEO',
-  image: 'wayne-barnett-founder-ceo.jpg'
-};
-console.log(person1);
+let personale = [
+ {
+  'name': 'Waine Barnett',
+  'role': 'Founder & CEO',
+  'image': 'wayne-barnett-founder-ceo.jpg'
+ },
 
-let person2 = {
-    name: 'Angela Caroll',
-    role: 'Chief Editor',
-    image: 'angela-caroll-chief-editor.jpg'
-  };
-  console.log(person2);
+ {
+    'name': 'Waine Barnett',
+    'role': 'Founder & CEO',
+    'image': 'wayne-barnett-founder-ceo.jpg'
+  },
 
-  let person3 = {
-    name: 'Walter Gordon',
-    role: 'Office Manager',
-    image: 'walter-gordon-office-manager.jpg'
-  };
-  console.log(person3);
-
-  let person4 = {
-    name: 'Angela Lopez',
-    role: 'Social Media Manager',
-    image: 'angela-lopez-social-media-manager.jpg'
-  };
-  console.log(person4);
+  {
+    'name': 'Waine Barnett',
+    'role': 'Founder & CEO',
+    'image': 'wayne-barnett-founder-ceo.jpg'
+  },
   
-  let person5 = {
-    name: 'Scott Estrada',
-    role: 'Developer',
-    image: 'scott-estrada-developer.jpg'
-  };
-  console.log(person5);
-
-  let person6= {
-    name: 'Barbara Ramos',
-    role: 'Graphic Designer',
-    image: 'barbara-ramos-graphic-designer.jpg'
-  };
-  console.log(person6);
+  {
+    'name': 'Waine Barnett',
+    'role': 'Founder & CEO',
+    'image': 'wayne-barnett-founder-ceo.jpg'
+  },
   
+  {
+    'name': 'Waine Barnett',
+    'role': 'Founder & CEO',
+    'image': 'wayne-barnett-founder-ceo.jpg'
+  },
+
+  {
+    'name': 'Waine Barnett',
+    'role': 'Founder & CEO',
+    'image': 'wayne-barnett-founder-ceo.jpg'
+  },
+  
+]
+console.log(personale);
