0e38422c8bd9f63e74d0996038b2d7901fdfb624
Anieb22 <anielloverdino@gmail.com>
2023-06-01 02:38:55 +0200

    creata una tabella con le info sul personale

--- js/javascript.js
+++ js/javascript.js
@@ -6,34 +6,54 @@ let personale = [
  },
 
  {
-    'name': 'Waine Barnett',
-    'role': 'Founder & CEO',
-    'image': 'wayne-barnett-founder-ceo.jpg'
+    'name': 'Angela Caroll',
+    'role': 'Chief Editor',
+    'image': 'angela-caroll-chief-editor.jpg'
   },
 
   {
-    'name': 'Waine Barnett',
-    'role': 'Founder & CEO',
-    'image': 'wayne-barnett-founder-ceo.jpg'
+    'name': 'Walter Gordon',
+    'role': 'Office Manager',
+    'image': 'walter-gordon-office-manager.jpg'
   },
   
   {
-    'name': 'Waine Barnett',
-    'role': 'Founder & CEO',
-    'image': 'wayne-barnett-founder-ceo.jpg'
+    'name': 'Angela Lopez',
+    'role': 'Social Media Manager',
+    'image': 'angela-lopez-social-media-manager.jpg'
   },
   
   {
-    'name': 'Waine Barnett',
-    'role': 'Founder & CEO',
-    'image': 'wayne-barnett-founder-ceo.jpg'
+    'name': 'Scot Estrada',
+    'role': 'Developer',
+    'image': 'scott-estrada-developer.jpg'
   },
 
   {
-    'name': 'Waine Barnett',
-    'role': 'Founder & CEO',
-    'image': 'wayne-barnett-founder-ceo.jpg'
+    'name': 'Barbara Ramos',
+    'role': 'Graphic Designer',
+    'image': 'barbara-ramos-graphic-designer.jpg'
   },
   
 ]
 console.log(personale);
+
+let tbody = document.getElementById('tbody');
+
+for (let i = 0; i < personale.length; i++) {
+  let tr = document.createElement('tr');
+  let nameCell = document.createElement('th');
+  nameCell.innerText = personale[i].name;
+  let roleCell = document.createElement('td');
+  roleCell.innerText = personale[i].role;
+  let imageCell = document.createElement('td');
+  let image = document.createElement('td');
+  image.innerText = personale[i].image
+  imageCell.appendChild(image);
+
+  tr.appendChild(nameCell);
+  tr.appendChild(roleCell);
+  tr.appendChild(imageCell);
+
+  tbody.appendChild(tr);
+}
